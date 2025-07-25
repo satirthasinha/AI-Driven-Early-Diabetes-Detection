86746481b3efc5ba99e6611517a030b60ac673b4
satirthasinha <satirtha_pg_24@cse.nits.ac.in>
2025-07-26 00:13:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from PIL import Image
 import qrcode
 from xgboost import XGBClassifier
 import shap
+from sklearn.model_selection import GridSearchCV
 
 # Page configuration
 st.set_page_config(page_title="Diabetes Predictor", layout="wide", initial_sidebar_state="expanded")
